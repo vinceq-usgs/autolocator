779efee208ed888a1d582981ce8e613eef0bb1b8
vinceq <vinceq@usgs.gov>
2016-06-08 01:03:26 +0000

    Add default list of producttypes to try, then create one string output

--- modules/geoserve.py
+++ modules/geoserve.py
@@ -1,14 +1,98 @@
+#! /usr/local/bin/python3
+
+import argparse
+from urllib import request
+import json
+
 """
-Geoserve URL query is this:
-http://earthquake.usgs.gov/ws/geoserve/regions.json?latitude=49.335&longitude=-76.9526&type=admin
+A module to access the USGS Geoserve Places Service.
 
-Geoserve raw output is this:
-{"metadata":{"request":"http:\/\/earthquake.usgs.gov\/ws\/geoserve\/regions.json?latitude=49.335&longitude=-76.9526&type=admin","submitted":"2016-06-07T00:06:09+00:00","types":["admin"],"version":"1.2.0"},"admin":{"type":"FeatureCollection","count":1,"features":[{"type":"Feature","id":68314,"geometry":null,"properties":{"iso":"CAN","country":"Canada","region":"Qu\u00e9bec"}}]}}
+Usage:
+    From the command line, geosoerve.py lat lon [gettype]
+    Or call from module:
+        import geoserve
+        resultsdict=geoserve.getname(lat,lon[,gettype])
+
+Results:
+
+    The server earthquake.usgs.gov/ws/geoserve gives different properties depending on gettype. If not specified, it will try 'admin', then 'fe'. 
+
+    admin: iso [country code e.g. USA],country,region [e.g. California]
+    fe: name,number
 
 """
 
-URL='http://earthquake.usgs.gov/ws/geoserve/regions.json?latitude={}&longitude={}&type={}'
+RAWURL='http://earthquake.usgs.gov/ws/geoserve/regions.json?latitude={}&longitude={}'
+GOODPRODUCTTYPES=[
+        {
+            'typename':'admin',
+            'proplist':['region','country']
+        },
+        {
+            'typename':'fe',
+            'proplist':['name']
+        }
+]
 
-def getname(lat,lon):
+def getname(lat,lon,gettype=''):
+    url=RAWURL.format(lat,lon,gettype)
+    producttypes=GOODPRODUCTTYPES
+    if gettype:
+        url+='&type={}'.format(gettype)
+        producttypes=[x for x in GOODPRODUCTTYPES if x['typename']==gettype]
+        if not producttypes:
+            print('WARNING: Unknown product type '+gettype)
+            return
+    
+    print('Accessing URL: '+url)
+    fh=request.urlopen(url)
+    data=fh.read().decode('utf8')
+    fh.close
+
+    results=json.loads(data)
+    if not results:
+        print('WARNING: No results found (no connection?)')
+        return
+
+    name=''
+    raw=''
+    for parse in producttypes:
+        typename=parse['typename']
+        if typename in results:
+            features=results[typename]['features']
+            if not features:
+                continue
+            props=features[0]['properties']
+            featurelist=[props[x] for x in parse['proplist']]
+            name=', '.join(featurelist)
+            break
+
+    if not name:
+        print('WARNING: No results found (bad location?)')
+        return
+
+    return({'raw':results,'name':name})
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(
+        description='Check the extended table for unknown entries and attempt to locate them.'
+    )
+    parser.add_argument('lat',type=float,
+        help='Latitude')
+    parser.add_argument('lon',type=float,
+        help='Longitude')
+    parser.add_argument('gettype',type=str,nargs='?',
+        help='Data type to query')
+
+    args=parser.parse_args()
+    results=getname(args.lat,args.lon,args.gettype)
+    print(results['name'])
+
+
+
+
+
+
+
   
 
